2cfa83a1ea665287e6deef00082302bf010da6a0
cguhring67 <c.guhring@fee.fr>
2025-02-20 03:16:03 +0100

    Ajout de l'interface d'administration grâce a EasyAdmin. Finalisation de l'interface d'ajout de films.

--- webapp/src/Services/FilmsGenres.php
+++ webapp/src/Services/FilmsGenres.php
@@ -8,27 +8,27 @@ class FilmsGenres
 	public function getGenres(): array
 	{
 		$genres = [
-			"Action",
-			"Animation",
-			"Arts Martiaux",
-			"Aventure",
-			"Biopic",
-			"Comédie",
-			"Comédie dramatique",
-			"Comédie musicale",
-			"Documentaire",
-			"Drame",
-			"Epouvante-horreur",
-			"Espionnage",
-			"Famille",
-			"Fantastique",
-			"Historique",
-			"Musical",
-			"Opera",
-			"Policier",
-			"Romance",
-			"Science fiction",
-			"Thriller"
+			"action"=>"Action",
+			"animation"=>"Animation",
+			"arts_martiaux" => "Arts Martiaux",
+			"aventure" => "Aventure",
+			"biopic" => "Biopic",
+			"comédie" => "Comédie",
+			"comédie_dramatique" => "Comédie dramatique",
+			"comédie_musicale" => "Comédie musicale",
+			"documentaire" => "Documentaire",
+			"drame" => "Drame",
+			"epouvante_horreur" => "Epouvante-horreur",
+			"espionnage" => "Espionnage",
+			"famille" => "Famille",
+			"fantastique" => "Fantastique",
+			"historique" => "Historique",
+			"musical" => "Musical",
+			"opera" => "Opera",
+			"policier" => "Policier",
+			"romance" => "Romance",
+			"science_fiction" => "Science fiction",
+			"thriller" => "Thriller"
 		];
 		return $genres;
 	}
